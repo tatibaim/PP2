c465591925a4e997cc55749ed83a8f2a42829cfd
tatibaim <tmiko0709@gmail.com>
2026-01-29 00:39:25 +0500

    ex1

---
+++ Week1/Practice 1/1-Python Output or Print.py
@@ -0,0 +1,10 @@
+#Default print
+print("Hello World!")
+
+#Double Quotes
+print("This will work!")
+print('This will also work!')
+
+#Print Without a New Line
+print("Hello World!", end=" ")
+print("I will print on the same line.")
