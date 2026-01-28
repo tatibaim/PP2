a0533f121f738c228cac37e7fc90b2ba8d8e6956
tatibaim <tmiko0709@gmail.com>
2026-01-29 00:41:06 +0500

    ex2

---
+++ Week1/Practice 1/2-Python Output Numbers.py
@@ -0,0 +1,11 @@
+#Print Numbers
+print(3)
+print(358)
+print(50000)
+
+#You can also do math inside the print() function:
+print(3 + 3)
+print(2 * 5)
+
+#Mix Text and Numbers
+print("I am", 35, "years old.")
