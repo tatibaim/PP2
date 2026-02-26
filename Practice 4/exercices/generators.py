f45ae841352358147b44e336371570688154e4d8
tatibaim <tmiko0709@gmail.com>
2026-02-26 12:18:10 +0500

    updated

--- Practice 4/exercices/generators.py
+++ Practice 4/exercices/generators.py
@@ -9,39 +9,43 @@ for x in squares(n):
     print(x)
 
 #2
+def even_numbers(n):
+    for i in range(0, n + 1):
+        if i % 2 == 0:
+            yield i
+
 n = int(input())
 
-def evens(n):
-    for i in range(0, n + 1, 2):
-        yield i
-
-first = True
-for x in evens(n):
-    if not first:
-        print(",", end="")
-    print(x, end="")
-    first = False
+print(",".join(str(x) for x in even_numbers(n)))
 
 #3
+def divisible(n):
+    for i in range(0, n + 1):
+        if i % 3 == 0 and i % 4 == 0:
+            yield i
+
 n = int(input())
 
-def evens(n):
-    for i in range(0, n + 1, 2):
-        yield i
-
-first = True
-for x in evens(n):
-    if not first:
-        print(",", end="")
-    print(x, end="")
-    first = False
+for num in divisible(n):
+    print(num)
 
 #4
-a, b = map(int, input().split())
-
 def squares(a, b):
     for i in range(a, b + 1):
         yield i * i
 
-for x in squares(a, b):
-    print(x)
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+
+for value in squares(a, b):
+    print(value)
+    
+#5
+def countdown(n):
+    for i in range(n, -1, -1):
+        yield i
+
+n = int(input())
+
+for num in countdown(n):
+    print(num)
