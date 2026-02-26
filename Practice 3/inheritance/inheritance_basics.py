986172e58648855e7494e1b8768b62971dbd7da2
tatibaim <tmiko0709@gmail.com>
2026-02-26 09:17:50 +0500

    comment added 2

--- Practice 3/inheritance/inheritance_basics.py
+++ Practice 3/inheritance/inheritance_basics.py
@@ -1,11 +1,18 @@
 # Basic inheritance
 
+# Create a base class Animal
 class Animal:
     def speak(self):
-        print("Some sound")
+        print("Some sound") # General behavior for all animals
 
+# Create a Dog class that inherites from Animal
 class Dog(Animal):
-    pass
+    pass # pass means: we won't add anything new for now
 
+# Create an object of the Dog class
 d = Dog()
-d.speak()
+
+# Call the speaking method
+# Although the Dog class doesn't have a speak method,
+# it's taken from the Animal class (the parent) 
+d.speak() # print Some sound
--- Practice 3/inheritance/method_overriding.py
+++ Practice 3/inheritance/method_overriding.py
@@ -1,12 +1,21 @@
 # Method overriding
 
+# Create a base class Animal
 class Animal:
+    # The speak method is "to make a sound"
     def speak(self):
         print("Animal sound")
 
+# Create a Cat class that inherits from Animal
 class Cat(Animal):
+    # Override the speak method
+    # Now Cat has its own version of this method
     def speak(self):
         print("Meow")
 
+# Create an object of the Cat class
 c = Cat()
-c.speak()
+
+# Call the speak method
+# The version from the Cat class is used, not the Animal version.
+c.speak() # print: Meow
