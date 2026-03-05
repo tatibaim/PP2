f11e2dcf2a4fd120fb3dce26e5e96b752839d4c7
tatibaim <tmiko0709@gmail.com>
2026-03-05 11:27:56 +0500

    10

--- Practice 5/regex/10.py
+++ Practice 5/regex/10.py
@@ -1,4 +1,6 @@
 import re
 txt = "CamelToSnake"
-x = re.sub(r"([A-Z])", r"_\1", txt).lower()
+x = re.sub("([A-Z])", r"_\1", txt).lower()
 print(x)
+# capture group
+# r"" raw string, so that \ works correctly
